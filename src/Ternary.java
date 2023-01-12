a253e371546222c9e8c1d739c2273f7bcc7129ec
connor <connor.jxnkins@gmail.com>
2023-01-12 01:07:53 +0000

    Changed name, added different classes for different subjects

---
+++ src/Ternary.java
@@ -0,0 +1,19 @@
+public class Ternary {
+    public static void main(String[] args){
+        // ternary
+        double numberOne = 20.00d;
+        double numberTwo = 80.00d;
+
+        double together = 100 * (numberOne + numberTwo);
+        double remainder = together / 40;
+
+        boolean isNoRemainder = remainder == 0;  // if remainder equals zero then testing is true, else its false
+
+        System.out.println(isNoRemainder);
+
+        if (!isNoRemainder) {
+            System.out.println("Got some remainder");
+        }
+
+    }
+}
